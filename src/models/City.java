db5015e2bea83f78a912cabbb71b4dca8c1d0fc4
Chess <34745596+Chessnl@users.noreply.github.com>
2018-06-18 14:10:18 +0200

    Added degrees

--- src/models/City.java
+++ src/models/City.java
@@ -8,18 +8,28 @@ public class City {
     private double longitude;
     private double latitude;
     private int timesVisited;
+    private int outdegree;
+    private int indegree;
+    private int totaldegree;
 
-    public City(String name, double longitude, double latitude, int timesVisited) {
+    public City(String name, double longitude, double latitude, int timesVisited, int out, int in, int tot) {
         this.name = name;
         this.longitude = longitude;
         this.latitude = latitude;
         this.timesVisited = timesVisited;
+        outdegree = out;
+        indegree = in;
+        totaldegree = tot;
     }
+
     public City() {
         this.name = "test";
         this.longitude = 0;
         this.latitude = 0;
         this.timesVisited = 0;
+        outdegree = 0;
+        indegree = 0;
+        totaldegree = 0;
     }
 
     public void setName(String name){
@@ -36,5 +46,11 @@ public class City {
     public double getLatitude() { return latitude; }
 
     public int getTimesVisited() { return timesVisited; }
+
+    public int getOutdegree() { return outdegree; }
+
+    public int getIndegree() { return indegree; }
+
+    public int getTotaldegree() { return totaldegree; }
 
 }
